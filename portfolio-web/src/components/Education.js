8367cd021e3b399ee15e1299779934e36b0aca3b
Yash Ray <ray.yash.08@gmail.com>
2021-10-04 15:09:04 +0530

    Education added

---
+++ portfolio-web/src/components/Education.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Education() {
+  return <div className='text-light-main'>EDUCATION</div>;
+}
+
+export default Education;
--- portfolio-web/src/pages/defaultScreen.js
+++ portfolio-web/src/pages/defaultScreen.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Navbar from "../components/navbar";
-import {Switch, Route} from 'react-router-dom';
-import Home from '../components/Home';
-import Projects from '../components/Projects';
+import { Switch, Route } from "react-router-dom";
+import Home from "../components/Home";
+import Projects from "../components/Projects";
+import Education from "../components/Education";
 
 function DefaultScreen() {
-    
-    return (
-        <div className='m-0 h-screen relative z-0 top-0 bg-dark-main overflow-hidden'>
-            <Navbar />
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/projects' component={Projects} />
-            </Switch>
-        </div>
-    )
+  return (
+    <div className="m-0 h-screen relative z-0 top-0 bg-dark-main overflow-hidden">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/education" component={Education} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </div>
+  );
 }
 
 export default DefaultScreen;
